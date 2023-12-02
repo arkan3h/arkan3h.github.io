78e611a5f7f6ee95162a8fb566afe01b689b4691
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2023-12-03 04:20:25 +0700

    Under dev

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Arkan_3h',
+      title: 'Arkan3h',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue.shade800),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Arkan_3h portofolio website'),
+      home: const MyHomePage(title: 'Arkan portfolio website'),
     );
   }
 }
@@ -48,40 +48,49 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.all(24),
-              child: const Text(
-                'You have pushed the button this many times:',
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 24),
-                  child: IconButton(onPressed: _minCounter, icon: const Icon(Icons.remove),)
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 24),
-                  child: IconButton(onPressed: _incrementCounter, icon: const Icon(Icons.add))
-                ),
-              ],
-            )
+          children: [
+            Text('Under Development'),
+            Text('@arkan3h')
           ],
         ),
       ),
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   title: Text(widget.title),
+      // ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       Container(
+      //         margin: const EdgeInsets.all(24),
+      //         child: const Text(
+      //           'You have pushed the button this many times:',
+      //         ),
+      //       ),
+      //       Row(
+      //         mainAxisAlignment: MainAxisAlignment.center,
+      //         children: [
+      //           Container(
+      //             margin: const EdgeInsets.only(right: 24),
+      //             child: IconButton(onPressed: _minCounter, icon: const Icon(Icons.remove),)
+      //           ),
+      //           Text(
+      //             '$_counter',
+      //             style: Theme.of(context).textTheme.headlineMedium,
+      //           ),
+      //           Container(
+      //             margin: const EdgeInsets.only(left: 24),
+      //             child: IconButton(onPressed: _incrementCounter, icon: const Icon(Icons.add))
+      //           ),
+      //         ],
+      //       )
+      //     ],
+      //   ),
+      // ),
     );
   }
 }
